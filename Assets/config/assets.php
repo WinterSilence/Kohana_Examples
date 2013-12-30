e76a8eef6dd7dca97a9c3ef6d6fdcd0758853984
WinterSilence <info@handy-soft.ru>
2013-12-31 00:59:12 +0400

    Assets

---
+++ Assets/config/assets.php
@@ -0,0 +1,9 @@
+<?php defined('SYSPATH') OR die('No direct script access.');
+
+return array(
+	'source_dir'  => 'media',
+	'ignore_exts' => array('php', 'gitignore'),
+	'ignore_dirs' => array('media/uploads'),
+	'assets_path' => DOCROOT.'assets',
+	'base_uri'    => 'assets/',
+);
