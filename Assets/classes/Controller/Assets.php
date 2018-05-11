cf2ae31a17f547fbb13897141f774016bbfe6c77
Anton <WinterSilence@users.noreply.github.com>
2018-05-11 09:16:48 +0300

    Update Assets.php

--- Assets/classes/Controller/Assets.php
+++ Assets/classes/Controller/Assets.php
@@ -1,3 +1,4 @@
-<?php defined('SYSPATH') OR die('No direct script access.');
+<?php
 
-class Controller_Assets extends Kohana_Controller_Assets {}
+class Controller_Assets extends Kohana_Controller_Assets {
+}
