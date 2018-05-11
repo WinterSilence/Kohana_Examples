71d8a2229496bc42dc1dd1f9ff93ec7344409a8a
Anton <WinterSilence@users.noreply.github.com>
2018-05-11 09:14:22 +0300

    Update Assets.php

--- Assets/classes/Kohana/Controller/Assets.php
+++ Assets/classes/Kohana/Controller/Assets.php
@@ -1,4 +1,4 @@
-<?php defined('SYSPATH') OR die('No direct script access.');
+<?php
 /**
  * Abstract assets controller class, uses for work with media files.
  *
@@ -11,9 +11,9 @@
 abstract class Kohana_Controller_Assets extends Controller {
 
 	/**
-	 * - Find file in secure storages.
+	 * - Find file in secure folder.
 	 * - Check path and file type.
-	 * - Copy file in public storage.
+	 * - Copy file in public folder.
 	 * - Display file.
 	 * 
 	 * @return void
@@ -30,52 +30,62 @@ abstract class Kohana_Controller_Assets extends Controller {
 
 		if (in_array($extension, $config['ignore_exts']))
 		{
-			throw HTTP_Exception::factory(415, 'Unsupported media type :name', array(':name' => $extension));
+			throw new HTTP_Exception(
+				415, 
+				'Unsupported media type :name', 
+				array(':name' => $extension)
+			);
 		}
 
-		// 
-		$filename = $dirname.DIR_SEPARATOR.$filename;
+		$filename = $dirname.DIRECTORY_SEPARATOR.$filename;
 		$source_file = Kohana::find_file($config['source_dir'], $filename, $extension);
 		
 		if (empty($source_file))
 		{
-			throw HTTP_Exception::factory(404, 'File :name not found', array(':name' => $filename));
+			throw new HTTP_Exception(
+				404, 
+				'File :name not found', 
+				array(':name' => $filename)
+			);
 		}
 		
 		foreach ($config['ignore_dirs'] as $dir)
 		{
-			if (UTF8::strpos($source_file, $dir) !== FALSE)
+			if (strpos($source_file, $dir) !== FALSE)
 			{
-				throw HTTP_Exception::factory(403, 'Access to file :name forbidden', 
-					array(':name' => Debug::path($source_file)));
+				throw new HTTP_Exception(
+					403, 
+					'Access to file :name forbidden', 
+					array(':name' => basename($source_file))
+				);
 			}
 		}
 		
 		// Get the application name from current request
 		$application = $this->request->param('application');
 		
-		$assets_path = $config['assets_path'].DIR_SEPARATOR.$application.DIR_SEPARATOR.$dirname;
+		$assets_path = $config['assets_path'].DIRECTORY_SEPARATOR.$application.DIRECTORY_SEPARATOR.$dirname;
 		
-		if ( ! is_dir($assets_path))
+		if (!file_exists($assets_path))
 		{
 			// Create a file path in a public storage
 			mkdir($assets_path, 0755, TRUE);
 		}
 		
-		$assets_file = $assets_path.DIR_SEPARATOR.$basename;
+		$assets_file = $assets_path.DIRECTORY_SEPARATOR.$basename;
 		
 		// Copy file in public storage
 		copy($source_file, $assets_file);
 		
+		$filetime = filemtime($assets_file);
 		// Check if the browser sent an "if-none-match: <etag>" header, and tell if the file hasn't changed
-		$this->check_cache(sha1($this->request->uri()).filemtime($assets_file));
+		$this->check_cache(sha1($this->request->uri()).$filetime);
 		// Send the file content as the response
 		$this->response->body(file_get_contents($assets_file));
 		// Set the proper headers to allow caching
 		$this->response->headers(array(
 			'content-type'  => File::mime_by_ext($extension),
-			'last-modified' => date('r', filemtime($assets_file)),
+			'last-modified' => date('r', $filetime),
 		));
 	}
-
-} // End Controller_Assets
+}
