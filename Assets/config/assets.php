aa6bce24d3ff2f1e4dc74384aab9a23b181d6f1a
Anton <WinterSilence@users.noreply.github.com>
2018-05-11 09:15:57 +0300

    Update assets.php

--- Assets/config/assets.php
+++ Assets/config/assets.php
@@ -2,7 +2,7 @@
 
 return array(
 	'source_dir'  => 'media',
-	'ignore_exts' => array('php', 'gitignore'),
+	'ignore_exts' => array('php', 'gitignore', 'htaccess', 'log'),
 	'ignore_dirs' => array('media/uploads'),
 	'assets_path' => DOCROOT.'assets',
 	'base_uri'    => 'assets/',
